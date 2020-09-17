ff6101cd96994ec84aaf78c00dfab2a9bfd1e13b
meizhiyh <meizhisazv@163.com>
2020-09-18 05:52:55 +0800

    Repaso de laravel

--- api-rest-laravel/routes/web.php
+++ api-rest-laravel/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/pruebas/{name?}', function($name = '') {
+    $texto = 'Hola ' . $name;
+    return $texto;
+});
