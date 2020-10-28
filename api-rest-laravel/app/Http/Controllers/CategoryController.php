e60eeec0a04ccff01cdff843b3f326bf8a8cbdb5
meizhiyh <meizhisazv@163.com>
2020-10-29 00:07:17 +0800

    api-larave: metodos para sacar categoria

--- api-rest-laravel/app/Http/Controllers/CategoryController.php
+++ api-rest-laravel/app/Http/Controllers/CategoryController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
 
 class CategoryController extends Controller
 {
-    public function pruebas(Request $request) {
-        return "Accion de pruebas category controller";
+    public function index() {
+        $categories = Category::all();
+
+        return response()->json([
+            'code' => 200,
+            'status' => 'success',
+            'categories' => $categories
+        ], 200);
+    }
+
+    public function show($id) {
+
+        $category = Category::find($id);
+
+        if(is_object($category)) {
+            $data = [
+                'code' => 200,
+                'status' => 'success',
+                'category' => $category
+            ];
+        } else {
+            $data = [
+                'code' => 404,
+                'status' => 'NotFound',
+                'message' => 'No se ha encontrado la categoria'
+            ];
+        }
+
+        return response()->json($data, $data['code']);
     }
 }
