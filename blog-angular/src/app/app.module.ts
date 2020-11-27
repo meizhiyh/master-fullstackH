4480f22d436e9850b752b46a146bc29ba4b5beef
meizhiyh <meizhisazv@163.com>
2020-11-27 11:39:09 +0800

    blog-angular: creacion de componentes del proyecto

--- blog-angular/src/app/app.module.ts
+++ blog-angular/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingProviders, routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
--- blog-angular/src/app/app.routing.ts
+++ blog-angular/src/app/app.routing.ts
@@ -5,10 +5,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { Route } from '@angular/compiler/src/core';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 
 const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: ErrorComponent },
 ];
 
 export const AppRoutingProviders: any[] = [];
