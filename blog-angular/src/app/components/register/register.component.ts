3d638479834b9dea49392ffea546e0e2ca8a6627
meizhiyh <meizhisazv@163.com>
2020-12-02 12:08:57 +0800

    blog-angular: formulario funcionalidad ngform

--- blog-angular/src/app/components/register/register.component.ts
+++ blog-angular/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   page_title: string;
+  user: User;
   constructor() {
+    this.user = new User(null, '', '', 'ROLE_USER', '', '', '', '');
     this.page_title = 'Registrate';
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form);
+    console.log(this.user);
   }
 
 }
