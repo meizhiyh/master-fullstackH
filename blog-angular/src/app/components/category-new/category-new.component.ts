362bb58cba71a68e78c659643f7851309e4c9885
meizhiyh <meizhisazv@163.com>
2020-12-30 20:42:38 +0800

    blog-angular: crear formulario de categoria

--- blog-angular/src/app/components/category-new/category-new.component.ts
+++ blog-angular/src/app/components/category-new/category-new.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { Category } from 'src/app/models/Category';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-category-new',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryNewComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string;
+  identity: any;
+  token: string;
+  category: Category;
+  status: string;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.pageTitle = 'Crear categoria';
+    this.identity = this.userService.gteIdentity();
+    this.token = this.userService.getToken();
+    this.category = new Category(null, '');
+
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form);
+    console.log(this.category);
   }
 
 }
