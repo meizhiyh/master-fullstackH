4cf795a2d697aa997b83fae0ff2d04bc0e8982c8
meizhiyh <meizhisazv@163.com>
2021-01-17 03:05:38 +0800

    blog-angular: crear servicio de post

---
+++ blog-angular/src/app/services/post.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/Post';
+import { global } from './global';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PostService {
+
+    private url: string;
+    private post: Post;
+
+    constructor(
+        private httpClient: HttpClient
+    ) {
+        this.url = global.url;
+    }
+
+    create(post: Post, token: string): Observable<any> {
+        const url = this.url + 'posts';
+        const headers = new HttpHeaders().set('Content-Type', 'application/json')
+         .set('Authorization', token);
+
+        return this.httpClient.post(url, post, {headers});
+    }
+}
