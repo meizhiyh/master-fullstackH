274669154a4f9a3dd0a207db964e31df57cfcb81
meizhiyh <meizhisazv@163.com>
2021-01-29 18:06:58 +0800

    blog-angular: conseguir datos de la entrada

--- blog-angular/src/app/components/post-detail/post-detail.component.ts
+++ blog-angular/src/app/components/post-detail/post-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/models/Post';
+import { PostService } from 'src/app/services/post.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailComponent implements OnInit {
 
-  constructor() { }
+  post: Post;
+
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getPost();
+  }
+
+  getPost(): void {
+    this.route.params.subscribe(params => {
+      const id = params['id'];
+
+      this.postService.getPost(id).subscribe(
+        response => {
+          if (response.status === 'success') {
+            this.post = response.post;
+            console.log(this.post);
+          } else {
+            this.router.navigate(['inicio']);
+          }
+        },
+        error => {
+          console.log(error);
+          this.router.navigate(['inicio']);
+        }
+      );
+    });
   }
 
 }
