4de8d92c9817de977a7ac66774a8ca52b354c4b4
meizhiyh <meizhisazv@163.com>
2021-02-14 10:16:54 +0800

    blog-angular: detalle de la categoria

--- blog-angular/src/app/components/category-detail/category-detail.component.ts
+++ blog-angular/src/app/components/category-detail/category-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Category } from '../../models/Category';
+import { CategoryService } from '../../services/category.service';
+import { global } from '../../services/global';
+import { Post } from 'src/app/models/Post';
 
 @Component({
   selector: 'app-category-detail',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryDetailComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  public category: Category;
+  public posts: Post[];
+  public url: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.url = global.url;
+    this.page_title = '';
+    this.getPostsByCategory();
   }
 
+  getPostsByCategory(): void {
+    this.route.params.subscribe(params => {
+      const id = +params['id'];
+
+      this.categoryService.getCategory(id).subscribe(
+        response => {
+          if (response.status === 'success') {
+            this.category = response.category;
+            this.page_title = this.category.name;
+          } else {
+            this.router.navigate(['inicio']);
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+
+      this.categoryService.getPosts(id).subscribe(
+        response => {
+          if (response.status === 'success') {
+            console.log(response);
+            this.posts = response.posts;
+          } else {
+            this.router.navigate(['inicio']);
+          }
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    });
+  }
 }
