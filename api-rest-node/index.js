08217f803973658ca4dad9f883df391c1d0397bc
meizhiyh <meizhisazv@163.com>
2021-03-07 03:11:05 +0800

    api-node: conexion a la base de datos

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -1,2 +1,10 @@
 'use strict'
 
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
+        .then(() => {
+            console.log('La conexion a la DB se ha efectuado correctamente!!');
+        })
+        .catch(error => console.log(error));
