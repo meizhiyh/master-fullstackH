5c9474b5b7f410456bdff0c982a6d22ff084ce77
meizhiyh <meizhisazv@163.com>
2021-03-14 21:28:21 +0800

    api-node: ruta de registro de usuario

--- api-rest-node/controllers/user.js
+++ api-rest-node/controllers/user.js
@@ -12,6 +12,31 @@ const controller = {
         return response.status(200).send({
             message: "Soy el metodo testeando"
         });
+    },
+     
+    save : function(req, res) {
+        // recooger los parametros de la peticion
+
+        // Validar los datos
+
+        // Crear el objeto del usuario
+
+        // Asignar valores al usuario
+
+        // Comprobar si el usuario existe
+
+        // Si no existe,
+
+        // cifrar la contrasena
+
+        // guardar usuario
+
+        // Devolver una respuesta
+
+        return res.status(200).send({
+            message: 'Registro de usuarios'
+        });
+
     }
 };
 
