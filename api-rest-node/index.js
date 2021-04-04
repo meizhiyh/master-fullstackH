e5ae2b10c5f0fc804b3db90c42a3af73100f8ec7
meizhiyh <meizhisazv@163.com>
2021-04-04 19:21:51 +0800

    api-node: mejoras en las consultas

--- api-rest-node/index.js
+++ api-rest-node/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const app = require('./app');
 const port = process.env.PORT || 3999;
 
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/api_rest_node', { useNewUrlParser: true })
         .then(() => {
