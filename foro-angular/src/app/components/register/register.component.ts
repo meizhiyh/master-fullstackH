d74597e3a4135e0fa25420e7e4eb37495c42872b
meizhiyh <meizhisazv@163.com>
2021-05-28 23:31:54 +0800

    foro-angular: preparando componente de registro

--- foro-angular/src/app/components/register/register.component.ts
+++ foro-angular/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  public user: User;
+
+  constructor() {
+    this.page_title = 'Registrate';
+    this.user = new User('', '', '', '', '', '', 'ROLE_USER');
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
