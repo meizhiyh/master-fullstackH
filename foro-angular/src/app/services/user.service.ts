c72e7e167a434a86c5ad76541762bfacfec4e78a
meizhiyh <meizhisazv@163.com>
2021-06-04 12:02:30 +0800

    foro-angular: crear metodo de registro

--- foro-angular/src/app/services/user.service.ts
+++ foro-angular/src/app/services/user.service.ts
@@ -2,15 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
+import { global } from './global';
 
 @Injectable()
 
 export class UserService {
+
+    private url: string;
+
     constructor(
         private _httpClient: HttpClient
-    ) {}
+    ) {
+        this.url = global.url;
+    }
 
     prueba(): void {
         console.log('Hola desde el servicio user');
+    }
+
+    register(user: User): Observable<any> {
+        const url = this.url + 'register';
+
+        const headres = new HttpHeaders().set(
+            'Content-Type', 'application/json'
+        );
+
+        return this._httpClient.post(url, user, { headers: headres });
     }
 }
