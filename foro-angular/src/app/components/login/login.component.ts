63ecdfd9309bb020d8ec21915f0a6c2a7062f6b6
meizhiyh <meizhisazv@163.com>
2021-06-07 09:35:08 +0800

    foro-angular: formulario de login

--- foro-angular/src/app/components/login/login.component.ts
+++ foro-angular/src/app/components/login/login.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public page_title: string;
+  public user: User;
+
+  constructor(
+    private _userService: UserService
+  ) {
+    this.page_title = 'Identificate';
+    this.user = new User('', '', '', '', '', '', 'ROLE_USER');
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log(form);
+    console.log(this.user);
   }
 
 }
